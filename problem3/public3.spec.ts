c61cd82e33127b631d9b663a1d201763590780c7
Fedor Zaytsev <fedor.zaytsev@hotmail.com>
2025-04-25 23:49:41 -0700

    solution3 score 268.47

--- problem3/public3.spec.ts
+++ problem3/public3.spec.ts
@@ -48,16 +48,19 @@ it('solution3', async () => {
         },
     );
     printTransactionFees(voteResult.transactions);
+    var totalGasFees = 0;
+    var totalGasUsed = 0;
     for (const tx of voteResult.transactions) {
         const computePhase = tx.description.computePhase.type === 'vm'
         ? tx.description.computePhase : undefined;
-        const actionPhase = tx.description.actionPhase.type === 'vm'
-        ? tx.description.actionPhase : undefined;
         console.log("tx gas fee", computePhase?.gasFees);
         console.log("tx gas used", computePhase?.gasUsed);
+        totalGasFees += Number(computePhase?.gasFees ?? 0n);
+        totalGasUsed += Number(computePhase?.gasUsed ?? 0n);
     }
     console.log("proposal contract balance after 0.1 ton vote", await blockchain.getContract(proposal.address).then(c => c.balance));
-
+    console.log("total gas fees", totalGasFees);
+    console.log("total gas used", totalGasUsed);
     // the vote was counted
     expect(await proposal.getProposalState()).toMatchObject({ yesCount: 1n, noCount: 0n });
 });
